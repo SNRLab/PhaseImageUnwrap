7605fffac85eb5a3e0f887c0ad7c49d86cdfc4ae
Franklin King <fking1@bwh.harvard.edu>
2023-07-26 10:30:30 -0400

    Initial upload

---
+++ PhaseImageUnwrap.py
@@ -0,0 +1,106 @@
+import os
+from pyexpat import model
+import unittest
+# from matplotlib.pyplot import get
+import vtk, qt, ctk, slicer
+from slicer.ScriptedLoadableModule import *
+import SimpleITK as sitk
+import sitkUtils
+from skimage.restoration import unwrap_phase
+from slicer.util import NodeModify
+import numpy as np
+from sys import platform
+
+class PhaseImageUnwrap(ScriptedLoadableModule):
+  """Uses ScriptedLoadableModule base class, available at:
+  https://github.com/Slicer/Slicer/blob/master/Bakse/Python/slicer/ScriptedLoadableModule.py
+  """
+
+  def __init__(self, parent):
+    ScriptedLoadableModule.__init__(self, parent)
+    self.parent.title = "Phase Image Unwrap"
+    self.parent.categories = ["Filtering"]
+    self.parent.dependencies = []
+    self.parent.contributors = ["Franklin King, Junichi Tokuda"]
+    self.parent.helpText = """
+"""
+    self.parent.helpText += self.getDefaultModuleDocumentationLink()
+    self.parent.acknowledgementText = """
+"""
+    # Set module icon from Resources/Icons/<ModuleName>.png
+    moduleDir = os.path.dirname(self.parent.path)
+    for iconExtension in ['.svg', '.png']:
+      iconPath = os.path.join(moduleDir, 'Resources/Icons', self.__class__.__name__ + iconExtension)
+      if os.path.isfile(iconPath):
+        parent.icon = qt.QIcon(iconPath)
+        break
+
+class PhaseImageUnwrapWidget(ScriptedLoadableModuleWidget):
+  """Uses ScriptedLoadableModuleWidget base class, available at:
+  https://github.com/Slicer/Slicer/blob/master/Base/Python/slicer/ScriptedLoadableModule.py
+  """
+
+  def __init__(self, parent=None):
+    ScriptedLoadableModuleWidget.__init__(self, parent)
+
+  def setup(self):
+    ScriptedLoadableModuleWidget.setup(self)
+
+    # IO collapsible button
+    IOCollapsibleButton = ctk.ctkCollapsibleButton()
+    IOCollapsibleButton.text = "Input/Output"
+    self.layout.addWidget(IOCollapsibleButton)
+
+    IOLayout = qt.QFormLayout(IOCollapsibleButton)
+
+    # Input scan plane transform
+    self.phaseImageSelector = slicer.qMRMLNodeComboBox()
+    self.phaseImageSelector.nodeTypes = ["vtkMRMLScalarVolumeNode"]
+    self.phaseImageSelector.selectNodeUponCreation = False
+    self.phaseImageSelector.noneEnabled = False
+    self.phaseImageSelector.addEnabled = False
+    self.phaseImageSelector.removeEnabled = True
+    self.phaseImageSelector.setMRMLScene(slicer.mrmlScene)
+    IOLayout.addRow("Phase Image: ", self.phaseImageSelector)
+
+    # MRI Start Updating scan target button
+    self.unwrapImageButton = qt.QPushButton("Unwrap Image")
+    self.unwrapImageButton.toolTip = "Create phase unwrap and gradient images"
+    IOLayout.addWidget(self.unwrapImageButton)
+    self.unwrapImageButton.connect('clicked()', self.onUnwrapImage)
+
+    spacer = qt.QSpacerItem(130, 10, qt.QSizePolicy.Maximum)
+    IOLayout.addSpacerItem(spacer)
+
+  def onUnwrapImage(self):
+    phaseImageNode = self.phaseImageSelector.currentNode()
+    self.phaseGradient(phaseImageNode)
+
+  def phaseGradient(self, inode):
+    image = sitk.Cast(sitkUtils.PullVolumeFromSlicer(inode), sitk.sitkFloat64)
+    scalarType = inode.GetImageData().GetScalarTypeAsString()
+    if scalarType == 'unsigned short':
+        print('imageBaseline*numpy.pi/2048.0 - numpy.pi')
+        imagePhase = image*np.pi/2048.0# - np.pi
+    else:
+        print('imageBaseline*numpy.pi/4096.0')
+        imagePhase = image*np.pi/4096.0 + np.pi
+
+    # Calculate gradient magnitude
+    imagePhaseUW = self.phaseUnwrap(imagePhase)
+    phaseUnwrapNodeName = inode.GetName() + '-phaseUnwrap'
+    sitkUtils.PushVolumeToSlicer(imagePhaseUW, name=phaseUnwrapNodeName, className='vtkMRMLScalarVolumeNode')
+
+    imagePhaseGrad = sitk.GradientMagnitude(imagePhaseUW)
+
+    phaseGradNodeName = inode.GetName() + '-phaseGrad'
+    sitkUtils.PushVolumeToSlicer(imagePhaseGrad, name=phaseGradNodeName, className='vtkMRMLScalarVolumeNode')
+  
+  def phaseUnwrap(self, imagePhase):
+    imagePhaseNP = sitk.GetArrayFromImage(imagePhase)
+    imageUnwrappedNP = unwrap_phase(imagePhaseNP)
+    imageUnwrapped = sitk.GetImageFromArray(imageUnwrappedNP)
+    imageUnwrapped.SetOrigin(imagePhase.GetOrigin())
+    imageUnwrapped.SetSpacing(imagePhase.GetSpacing())
+    imageUnwrapped.SetDirection(imagePhase.GetDirection())
+    return imageUnwrapped
